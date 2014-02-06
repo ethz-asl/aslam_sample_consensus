48ef78998f520f976643e1abb38bf6de7b939685
Paul Furgale <paul.furgale@gmail.com>
2014-02-06 19:46:08 +0100

    Removing cmake files

--- test/build/CMakeFiles/aslam_ransac_test_result.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/aslam_ransac_test_result"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/aslam_ransac_test_result.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
